01b68b82b9fd574528a253fe6c96c1df8240bf51
Konstantin Zaitsev <classicsvell@gmail.com>
2021-12-20 19:38:45 +0300

    Added comments, favicon

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const cookie = require('cookie-parser')
+const compression = require('compression')
+const helmet = require('helmet')
+const favicon = require('serve-favicon')
 
 require('dotenv').config()
 
@@ -19,21 +22,31 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+// MongoDB Cloud connection 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(app.listen(PORT))
     .catch(e => console.error(e))
 
 
 app.use(morgan('dev'))
+app.use(compression())
+app.use(helmet())
 
+// serving favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
+// parsing url body, JSON, cookie
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(cookie())
 
+// serving public files
 app.use(express.static('public'))
+// setting EJS view engine config
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
+// adding PORT for CodeRunController
 function addPort(req, res, next) {
     req.body.PORT = PORT
     next()
