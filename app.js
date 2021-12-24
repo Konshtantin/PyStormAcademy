fe0136ad4b3b62c41bbaeb6908166395b0545fb5
Konstantin Zaitsev <classicsvell@gmail.com>
2021-12-24 16:03:48 +0300

    Error port

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const {checkConfirm, checkNotLogin} = require('./middleware/authMiddleware')
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 80
 
 // MongoDB Cloud connection 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
