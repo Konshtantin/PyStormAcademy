2915c38303a8819f4ee921b1ecd5b5614e9ffbc6
Konstantin Zaitsev <classicsvell@gmail.com>
2021-12-26 14:59:03 +0300

    try http

--- app.js
+++ app.js
@@ -33,15 +33,16 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 
 app.use(morgan('dev'))
 
-// app.use(helmet.contentSecurityPolicy({
-//     useDefaults: true,
-//     directives: {
-//         "font-src": ['self', 'fonts.googleapis.com'],
-//         "script-src": ["self", "pystorm.xyz"],
-//         "style-src": ["self", "pystorm.xyz"]
-//     }
-// }))
-// app.use(helmet.hidePoweredBy())
+app.use(helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+        "font-src": ['self', 'fonts.googleapis.com'],
+        "script-src": "http://pystorm.xyz",
+        "style-src": "http://pystorm.xyz",
+        "img-src": "http://pystorm.xyz"
+    }
+}))
+app.use(helmet.hidePoweredBy())
 app.use(compression())
 
 // serving favicon
